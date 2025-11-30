483693365abf28e40207b379182cf4b15bfd2e05
João Darwin <joaodarwin.ip22@gmail.com>
2025-11-30 15:34:18 -0300

    feat: Criar método no repository de cupom para buscar cupom não deletado por id

--- src/main/java/com/coupon/api/repositories/CouponRepository.java
+++ src/main/java/com/coupon/api/repositories/CouponRepository.java
@@ -2,10 +2,15 @@ package com.coupon.api.repositories;
 
 import com.coupon.api.models.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+
+    @Query("SELECT coupon FROM Coupon coupon WHERE coupon.id = :id AND coupon.status != 'DELETED'")
+    Optional<Coupon> findByIdAndIsNotDelete(UUID id);
 }
