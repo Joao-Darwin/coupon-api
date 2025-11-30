b46b52c9963399397239a699e69de5280cf51647
João Darwin <joaodarwin.ip22@gmail.com>
2025-11-30 11:43:27 -0300

    feat: Tratar exceção da validação de campos

--- src/main/java/com/coupon/api/exceptions/handler/CustomizeExceptionHandler.java
+++ src/main/java/com/coupon/api/exceptions/handler/CustomizeExceptionHandler.java
@@ -3,12 +3,16 @@ package com.coupon.api.exceptions.handler;
 import com.coupon.api.dtos.exceptions.response.ResponseEntityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @RestController
@@ -19,5 +23,23 @@ public class CustomizeExceptionHandler {
         ResponseEntityException responseEntityException = new ResponseEntityException(Instant.now(), exception.getMessage(), webRequest.getDescription(false));
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseEntityException);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("moment", Instant.now());
+        response.put("message", "Algo está errado na validação dos campos");
+        response.put("details", ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> {
+                    Map<String, String> fieldError = new HashMap<>();
+                    fieldError.put("field", error.getField());
+                    fieldError.put("message", error.getDefaultMessage());
+                    return fieldError;
+                }).collect(Collectors.toList()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
