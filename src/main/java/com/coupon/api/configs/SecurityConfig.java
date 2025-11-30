cf01063123c686e8c31b6134e45e0a7c520bac8f
João Darwin <joaodarwin.ip22@gmail.com>
2025-11-30 09:56:08 -0300

    init: Criar base do projeto

---
+++ src/main/java/com/coupon/api/configs/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.coupon.api.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(corsOrigins.split(",")));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
