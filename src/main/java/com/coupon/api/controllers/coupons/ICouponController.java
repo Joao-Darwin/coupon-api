33bbe9a1a62597b63c30002189d00dd2e5f54ec2
João Darwin <joaodarwin.ip22@gmail.com>
2025-11-30 14:30:57 -0300

    feat: Criar endpoint de buscar cupom por id

--- src/main/java/com/coupon/api/controllers/coupons/ICouponController.java
+++ src/main/java/com/coupon/api/controllers/coupons/ICouponController.java
@@ -4,6 +4,9 @@ import com.coupon.api.dtos.coupons.request.CreateCouponDTO;
 import com.coupon.api.dtos.coupons.response.CouponDTO;
 import org.springframework.http.ResponseEntity;
 
+import java.util.UUID;
+
 public interface ICouponController {
     ResponseEntity<CouponDTO> create(CreateCouponDTO createCouponDTO);
+    ResponseEntity<CouponDTO> findById(UUID id);
 }
--- src/main/java/com/coupon/api/controllers/coupons/impl/CouponController.java
+++ src/main/java/com/coupon/api/controllers/coupons/impl/CouponController.java
@@ -8,10 +8,14 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("${base-path}/coupons")
@@ -29,5 +33,12 @@ public class CouponController implements ICouponController {
         CouponDTO couponCreated = couponService.create(createCouponDTO);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(couponCreated);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CouponDTO> findById(@PathVariable UUID id) {
+        CouponDTO coupon = couponService.findById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(coupon);
     }
 }
