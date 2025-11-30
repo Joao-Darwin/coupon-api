0a00f09a6025e029b8da6ce41e6e12fa6d8744a5
João Darwin <joaodarwin.ip22@gmail.com>
2025-11-30 13:38:39 -0300

    test: Criar teste para endpoint de criar cupom

---
+++ src/test/java/com/coupon/api/controllers/CouponControllerTest.java
@@ -0,0 +1,87 @@
+package com.coupon.api.controllers;
+
+import com.coupon.api.controllers.coupons.impl.CouponController;
+import com.coupon.api.dtos.coupons.request.CreateCouponDTO;
+import com.coupon.api.dtos.coupons.response.CouponDTO;
+import com.coupon.api.models.enums.CouponStatus;
+import com.coupon.api.services.CouponService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@WebMvcTest(controllers = CouponController.class)
+public class CouponControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Value("${base-path}/coupons")
+    private String basePath;
+
+    @MockitoBean
+    private CouponService couponService;
+
+    @Test
+    void testCreate_ShouldReturnCreatedHttpCodeWithCreatedCouponOnBody() throws Exception {
+        String code = "ABC-123";
+        String cleanCode = "ABC123";
+        String description = "Foo Bar";
+        Double discountValue = 0.8;
+        LocalDateTime expirationDate = LocalDateTime.of(2050, 12, 1, 5, 5, 15);
+        CouponStatus status = CouponStatus.ACTIVE;
+        boolean published = false;
+        boolean redeemed = false;
+
+        CreateCouponDTO createCouponDTO = new CreateCouponDTO(
+                code,
+                description,
+                discountValue,
+                expirationDate,
+                published
+        );
+
+        CouponDTO couponDTO = new CouponDTO(
+                UUID.randomUUID(),
+                cleanCode,
+                description,
+                discountValue,
+                expirationDate,
+                status,
+                published,
+                redeemed
+        );
+
+        Mockito.when(couponService.create(Mockito.any(CreateCouponDTO.class))).thenReturn(couponDTO);
+
+        ResultActions result = mockMvc
+                .perform(MockMvcRequestBuilders.post(basePath)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(createCouponDTO)));
+
+        result.andExpect(MockMvcResultMatchers.status().isCreated());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(couponDTO.id().toString()));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(cleanCode));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.description").value(description));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.discountValue").value(discountValue));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.expirationDate").value(expirationDate.toString()));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(status.toString()));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.published").value(published));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.redeemed").value(redeemed));
+
+        Mockito.verify(couponService).create(Mockito.any(CreateCouponDTO.class));
+    }
+}
